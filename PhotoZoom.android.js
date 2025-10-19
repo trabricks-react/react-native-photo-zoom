fc7493713982557ac5bd729c77658c4eb406048c
moonsuhan <m@doa.team>
2025-10-19 12:32:11 +0900

    11

--- PhotoZoom.android.js
+++ PhotoZoom.android.js
@@ -1,90 +1,88 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { requireNativeComponent } from 'react-native';
+import {requireNativeComponent} from 'react-native';
 import ViewPropTypes from 'react-native/Libraries/Components/View/ViewPropTypes';
 
-const resolveAssetSource = require('react-native/Libraries/Image/resolveAssetSource');
-
 export default class PhotoZoom extends Component {
-  static propTypes = {
-    source: PropTypes.oneOfType([
-      PropTypes.shape({
-        uri: PropTypes.string
-      }),
-      // Opaque type returned by require('./image.jpg')
-      PropTypes.number
-    ]),
-    loadingIndicatorSource: PropTypes.oneOfType([
-      PropTypes.shape({
-        uri: PropTypes.string
-      }),
-      // Opaque type returned by require('./image.jpg')
-      PropTypes.number
-    ]),
-    fadeDuration: PropTypes.number,
-    minimumZoomScale: PropTypes.number,
-    maximumZoomScale: PropTypes.number,
-    scale: PropTypes.number,
-    androidZoomTransitionDuration: PropTypes.number,
-    androidScaleType: PropTypes.oneOf(["center", "centerCrop", "centerInside", "fitCenter", "fitStart", "fitEnd", "fitXY", "matrix"]),
-    onLoadStart: PropTypes.func,
-    onError: PropTypes.func,
-    onLoad: PropTypes.func,
-    onLoadEnd: PropTypes.func,
-    onTap: PropTypes.func,
-    onViewTap: PropTypes.func,
-    onScale: PropTypes.func,
-    ...ViewPropTypes
-  };
+    static propTypes = {
+        source: PropTypes.oneOfType([
+            PropTypes.shape({
+                uri: PropTypes.string
+            }),
+            // Opaque type returned by require('./image.jpg')
+            PropTypes.number
+        ]),
+        loadingIndicatorSource: PropTypes.oneOfType([
+            PropTypes.shape({
+                uri: PropTypes.string
+            }),
+            // Opaque type returned by require('./image.jpg')
+            PropTypes.number
+        ]),
+        fadeDuration: PropTypes.number,
+        minimumZoomScale: PropTypes.number,
+        maximumZoomScale: PropTypes.number,
+        scale: PropTypes.number,
+        androidZoomTransitionDuration: PropTypes.number,
+        androidScaleType: PropTypes.oneOf(["center", "centerCrop", "centerInside", "fitCenter", "fitStart", "fitEnd", "fitXY", "matrix"]),
+        onLoadStart: PropTypes.func,
+        onError: PropTypes.func,
+        onLoad: PropTypes.func,
+        onLoadEnd: PropTypes.func,
+        onTap: PropTypes.func,
+        onViewTap: PropTypes.func,
+        onScale: PropTypes.func,
+        ...ViewPropTypes
+    };
 
-  render() {
-    const source = resolveAssetSource(this.props.source);
-    var loadingIndicatorSource = resolveAssetSource(this.props.loadingIndicatorSource);
+    render() {
+        const source = this.props.source;
+        var loadingIndicatorSource = this.props.loadingIndicatorSource;
 
-    if (source && source.uri === '') {
-      console.warn('source.uri should not be an empty string');
+        if (source && source.uri === '') {
+            console.warn('source.uri should not be an empty string');
+        }
+
+        if (this.props.src) {
+            console.warn('The <PhotoZoom> component requires a `source` property rather than `src`.');
+        }
+
+        if (source && source.uri) {
+            var {onLoadStart, onLoad, onLoadEnd, onTap, onViewTap, onScale, onError, ...props} = this.props;
+
+            var nativeProps = {
+                onPhotoZoomerError: onError,
+                onPhotoZoomerLoadStart: onLoadStart,
+                onPhotoZoomerLoad: onLoad,
+                onPhotoZoomerLoadEnd: onLoadEnd,
+                onPhotoZoomerTap: onTap,
+                onPhotoZoomerViewTap: onViewTap,
+                onPhotoZoomerScale: onScale,
+                ...props,
+                shouldNotifyLoadEvents: !!(onLoadStart || onLoad || onLoadEnd || onError),
+                src: source,
+                loadingIndicatorSrc: loadingIndicatorSource ? loadingIndicatorSource.uri : null,
+            };
+
+            return <PhotoZoomAndroid {...nativeProps} />
+        }
+        return null
     }
-
-    if (this.props.src) {
-      console.warn('The <PhotoZoom> component requires a `source` property rather than `src`.');
-    }
-
-    if (source && source.uri) {
-      var { onLoadStart, onLoad, onLoadEnd, onTap, onViewTap, onScale, onError, ...props } = this.props;
-
-      var nativeProps = {
-        onPhotoZoomerError: onError,
-        onPhotoZoomerLoadStart: onLoadStart,
-        onPhotoZoomerLoad: onLoad,
-        onPhotoZoomerLoadEnd: onLoadEnd,
-        onPhotoZoomerTap: onTap,
-        onPhotoZoomerViewTap: onViewTap,
-        onPhotoZoomerScale: onScale,
-        ...props,
-        shouldNotifyLoadEvents: !!(onLoadStart || onLoad || onLoadEnd || onError),
-        src: source,
-        loadingIndicatorSrc: loadingIndicatorSource ? loadingIndicatorSource.uri : null,
-      };
-
-      return <PhotoZoomAndroid {...nativeProps} />
-    }
-    return null
-  }
 }
 
 var cfg = {
-  nativeOnly: {
-    onPhotoZoomerError: true,
-    onPhotoZoomerLoadStart: true,
-    onPhotoZoomerLoad: true,
-    onPhotoZoomerLoadEnd: true,
-    onPhotoZoomerTap: true,
-    onPhotoZoomerViewTap: true,
-    onPhotoZoomerScale: true,
-    shouldNotifyLoadEvents: true,
-    src: true,
-    loadingIndicatorSrc: true
-  }
+    nativeOnly: {
+        onPhotoZoomerError: true,
+        onPhotoZoomerLoadStart: true,
+        onPhotoZoomerLoad: true,
+        onPhotoZoomerLoadEnd: true,
+        onPhotoZoomerTap: true,
+        onPhotoZoomerViewTap: true,
+        onPhotoZoomerScale: true,
+        shouldNotifyLoadEvents: true,
+        src: true,
+        loadingIndicatorSrc: true
+    }
 };
 
 const PhotoZoomAndroid = requireNativeComponent('PhotoZoomAndroid', PhotoZoom, cfg);
